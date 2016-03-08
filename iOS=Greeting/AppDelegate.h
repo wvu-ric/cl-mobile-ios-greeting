9e97f71a019f558d7ea574449350c8d77b9c43c2
rickykirkendall <rickykirkendall@flocoapps.com>
2016-03-07 19:37:51 -0500

    Initial Commit

---
+++ iOS=Greeting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS=Greeting
+//
+//  Created by Ricky Kirkendall on 3/7/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS=Greeting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS=Greeting
+//
+//  Created by Ricky Kirkendall on 3/7/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
